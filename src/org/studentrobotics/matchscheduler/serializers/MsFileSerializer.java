e61b9b4bdcbe60dc0f9d3483f3021e405147f3f0
Sam Phippen <samphippen@googlemail.com>
2011-04-06 21:35:54 +0100

    add a parse implementation for msfileserializer
    this is the primary way rescheduling is implemented
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/org/studentrobotics/matchscheduler/serializers/MsFileSerializer.java
+++ src/org/studentrobotics/matchscheduler/serializers/MsFileSerializer.java
@@ -62,7 +62,7 @@ public class MsFileSerializer implements MatchSerializer {
 
                 }
                 
-                d = getDateForMatchNumber(matchCount++);
+                d = getDateForMatchNumber(++matchCount);
 
                 ps.print("\n");
             }
@@ -73,6 +73,34 @@ public class MsFileSerializer implements MatchSerializer {
             e.printStackTrace();
         }
 
+    }
+
+    public List<Match> parse(String fileName) throws FileNotFoundException {
+        File f = new File(fileName);
+        byte[] fileBytes = new byte[(int)f.length()];
+        FileReader fr = new FileReader(f);
+        List<Match> result = new ArrayList<Match>();
+        for (int i = 0; i < fileBytes.length; i++) {
+            try {
+                fileBytes[i] = (byte)fr.read();
+                
+                
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        
+        String s = new String(fileBytes);
+        String[] matchSpecs = s.split("\n");
+        int i = 0;
+        for (String line : matchSpecs) {
+            result.add(Match.parse(line, i++));
+        }
+        
+        
+        return result;
     }
 
 }
