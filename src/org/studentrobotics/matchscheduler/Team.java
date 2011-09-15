31da895e3862eff7dde169711d48a887f0dbac7f
Sam Phippen <samphippen@googlemail.com>
2011-09-15 22:54:25 +0100

    removed some unused imports from Team.java
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- src/org/studentrobotics/matchscheduler/Team.java
+++ src/org/studentrobotics/matchscheduler/Team.java
@@ -2,9 +2,7 @@ package org.studentrobotics.matchscheduler;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
